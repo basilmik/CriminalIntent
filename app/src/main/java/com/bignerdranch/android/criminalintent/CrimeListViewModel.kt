abdff5650259467ae8914b8871ad9641fc987afb
basil <basil@BASILIK>
2022-06-21 22:06:57 +0300

    chapter 9 first half of the code

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel(){
+
+    val crimes = mutableListOf<Crime>()
+    init{
+
+        for (i in 0 until 100)
+        {
+            val crime = Crime()
+            crime.title = "Crime №$i"
+            crime.isSolved = 1 % 2 == 0
+            crimes += crime
+        }
+    }
+
+}
